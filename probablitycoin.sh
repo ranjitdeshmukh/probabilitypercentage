f67f8199caa7ffc1d58d9e8c71867f932008b91d
Shubham <s.shrinath@splendornet.com>
2020-05-28 00:07:40 +0530

    Added : Singlet Combination.

--- probablitycoin.sh
+++ probablitycoin.sh
@@ -1,11 +1,45 @@
+#!/bin/bash -x
 echo "Welcome to Probablity Problem"
 
 
-choice=$((RANDOM % 2 ))
+declare -A dict
 
-if [ $choice -eq "1" ]
+read -p "Enter the value coin flip : " flip
+read varname
+for (( i = 0; i <$flip; i++ )); 
+do
+	choice=$((RANDOM % 2 ))
+
+	if [ $choice -eq "1" ]
+	then
+		head=`expr $head + 1`
+
+	elif [ $choice -eq "0" ]
+	then 
+		tail=`expr $tail + 1`
+	fi
+done
+
+dict+=([Key1]=$head [Key2]=$tail)
+
+
+function checkProbablity(){
+	a=$1
+    b=$2
+    result=$(( 100 * $a / $b )) 
+	echo "$result" 
+}
+
+if [[ $varname -eq "H" ]]; 
 then
-	echo "Heads"
-else 
-	echo "tail"
+	a=$head
+    b=$flip
+    new=$( checkProbablity $a $b )	
+    echo "$new%"
+elif [[ $varname -eq "T" ]] 
+then
+	a=$head
+    b=$flip
+    new=$( checkProbablity $a $b )	
+    echo "$new%"
 fi
